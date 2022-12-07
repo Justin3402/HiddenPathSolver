f5398345149a1255c617ce530ed2a6c85e821190
Justin <justin-robitaille@hotmail.com>
2022-12-06 20:14:30 -0500

    Python test

---
+++ main.py
@@ -0,0 +1,10 @@
+def greetings(name):
+    print(f'Hi, {name}')
+
+def start():
+    print("Starting...")
+
+def handle_click(e):
+    print("you clicked the button")
+
+# greetings('you')
---
+++ show.py
@@ -0,0 +1,18 @@
+import cgi, os
+
+print('content-type:text/html\r\n\r\n')
+
+form = cgi.FieldStorage()
+pn = str(form.getvalue("pname"))
+des = str(form.getvalue("des"))
+fle = form['filename']
+
+fn = os.path.basename(fle.filename)
+open("C:/Users/Justin/Documents/GitHub/HiddenPathSolver/temp" + fn, "wb").write(fle.file.read())
+
+print('<html>')
+print('<body><center>')
+print('<h1>Product Name\n(%s)</h1>'%pn)
+print('<img src=temp/%s>'%fn)
+print('<h2>%s</h2>'%des)
+print('</center></body></html>')
