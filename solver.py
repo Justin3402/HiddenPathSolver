9aae6a826c0c7d32b9ab9c5f4d971ba081770b4c
Justin <justin-robitaille@hotmail.com>
2022-12-07 21:44:59 -0500

    Working with a predefined board

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 import numpy as np
+# from pyscript import *
 # from utils import add_class, remove_class
 
 # tasks = []
@@ -33,7 +34,7 @@ previous_col_end = 3
 row_pos = row_start
 col_pos = col_start
 
-path = np.zeros((height, width))
+path = np.zeros((height, width), dtype=int)
 grid = np.zeros((height, width))
 
 
@@ -92,7 +93,7 @@ def move(board, direction):
             row_pos = row_des
             col_pos = col_des
             return True  # A surrounding box is available
-    return False  # No surrounding box available
+    return False  # No surrounding box is available
 
 
 def solve_board(board):
@@ -137,6 +138,7 @@ def solve(pathData):
     global row_end
     global col_end
     global iterations
+    global is_solved
 
     row_start = pathData[0]
     col_start = pathData[1]
@@ -156,54 +158,253 @@ def solve(pathData):
 
     if solve_board(board):
         pyscript.write('solveStatus', f'The board is solved! {iterations} iterations were needed.')
-        # pyscript.write('finalGrid', f'The path is:\r\n {display_grid(path, False)}')
-        pyscript.write('finalGrid', 'The path is:')
-        pyscript.write('o1', int(path[0][0]))
-        pyscript.write('o2', int(path[0][1]))
-        pyscript.write('o3', int(path[0][2]))
-        pyscript.write('o4', int(path[0][3]))
-        pyscript.write('o5', int(path[1][0]))
-        pyscript.write('o6', int(path[1][1]))
-        pyscript.write('o7', int(path[1][2]))
-        pyscript.write('o8', int(path[1][3]))
-        pyscript.write('o9', int(path[2][0]))
-        pyscript.write('o10', int(path[2][1]))
-        pyscript.write('o11', int(path[2][2]))
-        pyscript.write('o12', int(path[2][3]))
-        pyscript.write('o13', int(path[3][0]))
-        pyscript.write('o14', int(path[3][1]))
-        pyscript.write('o15', int(path[3][2]))
-        pyscript.write('o16', int(path[3][3]))
+        pyscript.write('finalGrid', f'The path is:\r\n {display_grid(path, False)}')
     else:
         pyscript.write('solveStatus', 'The board could not be solved :(')
 
 
-def getData():
-    for row in range(height):
-        for column in range(width):
-            grid[row][column] = Element(f'i{4*row+column+1}').element.value
-    # newNumber = Element("i1").element.value
-    # grid[0][0] = newNumber
-    # pyscript.write('test1', newNumber)
-    # newNumber = Element("i2").element.value
-    # grid[0][1] = newNumber
-    # pyscript.write('test2', newNumber)
-    # newNumber = Element("i3").element.value
-    # grid[0][2] = newNumber
-    # pyscript.write('test3', newNumber)
-    # newNumber = Element("i4").element.value
-    # grid[0][3] = newNumber
-    # pyscript.write('test4', newNumber)
-
-
 def handle_click(e):
-    # pyscript.write("output", "you clicked the button")
-    getData()
     inputTable = ([10, 4, 10, 6], [10, 4, 5, 1], [5, 6, 1, 2], [7, 5, 7, 5])
     targetSum = 44
-
-    pyscript.write('inputGrid', f'The starting grid is:\r\n {display_grid(grid, False)}')
-
-    # pathStartData = [rowStart, colStart, rowEnd, colEnd, targetSum, inputTable]
-    pathStartData = [2, 0, 3, 3, targetSum, grid]
+    pathStartData = [2, 0, 3, 3, targetSum, inputTable]
     solve(pathStartData)
+
+
+
+
+# def solve_board(board):
+#     global grid
+#     global path
+#     global target
+#     global row_pos
+#     global col_pos
+#     global iterations
+#     global is_solved
+
+#     # pyscript.write('solveStatus', f'{iterations} iterations')
+
+
+#     row = board[0]
+#     col = board[1]
+#     total = board[2]
+#     step = board[3]
+#     pyscript.write('solveStatus', 'Test 1')
+
+#     step = step + 1
+#     pyscript.write('solveStatus', 'Test 2')
+#     pyscript.write('solveStatus', f'path = {path}')
+#     path[row][col] = step
+#     pyscript.write('solveStatus', 'Test 3')
+#     total = total + grid[row][col]
+#     pyscript.write('solveStatus', 'Test 4')
+#     board = [row, col, total, step]
+#     pyscript.write('solveStatus', 'Test 5')
+#     if total == target and row == row_end and col == col_end:
+#         is_solved = 1
+#         return True
+
+#     for i in range(0, 4):
+#         iterations = iterations + 1
+#         if move(board, i):
+#             new_board = [row_pos, col_pos, total, step]
+#             if solve_board(new_board):
+#                 return True
+#     path[row][col] = 0
+#     return False  # No more move remaining
+
+
+# def solve(pathData):
+#     global grid
+#     global path
+#     global target
+#     global row_pos
+#     global col_pos
+#     global row_start
+#     global col_start
+#     global row_end
+#     global col_end
+#     global iterations
+
+#     row_start = pathData[0]
+#     col_start = pathData[1]
+#     row_end = pathData[2]
+#     col_end = pathData[3]
+#     target = pathData[4]
+#     grid = pathData[5]
+
+#     row_pos = row_start
+#     col_pos = col_start
+
+#     path = np.zeros((height, width))
+#     board = [row_start, col_start, 0, 0]
+
+#     iterations = 0
+#     is_solved = 0
+
+#     if solve_board(board):
+#         pyscript.write('solveStatus', f'The board is solved! {iterations} iterations were needed.')
+#         # pyscript.write('finalGrid', f'The path is:\r\n {display_grid(path, False)}')
+#         pyscript.write('finalGrid', 'The path is:')
+#         pyscript.write('o1', int(path[0][0]))
+#         pyscript.write('o2', int(path[0][1]))
+#         pyscript.write('o3', int(path[0][2]))
+#         pyscript.write('o4', int(path[0][3]))
+#         pyscript.write('o5', int(path[1][0]))
+#         pyscript.write('o6', int(path[1][1]))
+#         pyscript.write('o7', int(path[1][2]))
+#         pyscript.write('o8', int(path[1][3]))
+#         pyscript.write('o9', int(path[2][0]))
+#         pyscript.write('o10', int(path[2][1]))
+#         pyscript.write('o11', int(path[2][2]))
+#         pyscript.write('o12', int(path[2][3]))
+#         pyscript.write('o13', int(path[3][0]))
+#         pyscript.write('o14', int(path[3][1]))
+#         pyscript.write('o15', int(path[3][2]))
+#         pyscript.write('o16', int(path[3][3]))
+#     else:
+#         pyscript.write('solveStatus', 'The board could not be solved :(')
+
+
+# def getData():
+#     global height
+#     global width
+#     global grid
+#     global path
+#     global target
+#     global row_pos
+#     global col_pos
+#     global row_start
+#     global col_start
+#     global row_end
+#     global col_end
+#     global iterations
+#     global is_solved
+
+#     pyscript.write('solveStatus', 'Getting the grid')
+#     for row in range(height):
+#         for column in range(width):
+#             grid[row][column] = Element(f'i{4*row+column+1}').element.value
+    
+#     pyscript.write('solveStatus', 'Getting the parameters')
+#     row_start = Element('startRow').element.value
+#     col_start = Element('startCol').element.value
+#     row_end = Element('endRow').element.value
+#     col_end = Element('endCol').element.value
+#     target = Element('targetSum').element.value
+
+#     pyscript.write('solveStatus', 'Some computing')
+#     row_pos = row_start
+#     col_pos = col_start
+
+#     path = np.zeros((height, width), dtype=int)
+#     board = [row_start, col_start, 0, 0]
+
+#     iterations = 0
+#     is_solved = 0
+
+#     pyscript.write('solveStatus', 'Returning the board')
+#     return board
+
+
+# def handle_click(e):
+#     global grid
+#     global path
+
+#     board = getData()
+#     pyscript.write('solveStatus', 'Data acquired!')
+#     # pyscript.write('inputGrid', f'The starting grid is:\r\n {display_grid(grid, False)}')
+    
+#     pyscript.write('solveStatus', 'Solving...')
+#     if solve_board(board):
+#         pyscript.write('solveStatus', f'The board is solved! {iterations} iterations were needed.')
+#         # pyscript.write('finalGrid', f'The path is:\r\n {display_grid(path, False)}')
+#         pyscript.write('finalGrid', 'The path is:')
+#         pyscript.write('o1', int(path[0][0]))
+#         pyscript.write('o2', int(path[0][1]))
+#         pyscript.write('o3', int(path[0][2]))
+#         pyscript.write('o4', int(path[0][3]))
+#         pyscript.write('o5', int(path[1][0]))
+#         pyscript.write('o6', int(path[1][1]))
+#         pyscript.write('o7', int(path[1][2]))
+#         pyscript.write('o8', int(path[1][3]))
+#         pyscript.write('o9', int(path[2][0]))
+#         pyscript.write('o10', int(path[2][1]))
+#         pyscript.write('o11', int(path[2][2]))
+#         pyscript.write('o12', int(path[2][3]))
+#         pyscript.write('o13', int(path[3][0]))
+#         pyscript.write('o14', int(path[3][1]))
+#         pyscript.write('o15', int(path[3][2]))
+#         pyscript.write('o16', int(path[3][3]))
+#     else:
+#         pyscript.write('solveStatus', 'The board could not be solved :(')
+
+
+# def handle_click(e):
+#     global height
+#     global width
+#     global grid
+#     global path
+#     global target
+#     global row_pos
+#     global col_pos
+#     global row_start
+#     global col_start
+#     global row_end
+#     global col_end
+#     global iterations
+#     global is_solved
+
+#     pyscript.write('solveStatus', 'Getting the grid')
+#     for row in range(height):
+#         for column in range(width):
+#             grid[row][column] = Element(f'i{4*row+column+1}').element.value
+    
+#     pyscript.write('solveStatus', 'Getting the parameters')
+#     row_start = Element('startRow').element.value
+#     pyscript.write('o1', row_start)
+#     col_start = Element('startCol').element.value
+#     pyscript.write('o2', col_start)
+#     row_end = Element('endRow').element.value
+#     pyscript.write('o3', row_end)
+#     col_end = Element('endCol').element.value
+#     pyscript.write('o4', col_end)
+#     target = Element('targetSum').element.value
+#     pyscript.write('o5', target)
+
+#     pyscript.write('solveStatus', 'Preparing..')
+#     row_pos = row_start
+#     col_pos = col_start
+
+#     path = np.zeros((height, width))
+#     board = [row_start, col_start, 0, 0]
+
+#     iterations = 0
+#     is_solved = 0
+
+#     # board = getData()
+#     pyscript.write('solveStatus', 'Data acquired!')
+#     pyscript.write('inputGrid', f'The starting grid is:\r\n {display_grid(grid, False)}')
+    
+#     pyscript.write('solveStatus', 'Solving...')
+#     if solve_board(board):
+#         pyscript.write('solveStatus', f'The board is solved! {iterations} iterations were needed.')
+#         # pyscript.write('finalGrid', f'The path is:\r\n {display_grid(path, False)}')
+#         pyscript.write('finalGrid', 'The path is:')
+#         pyscript.write('o1', int(path[0][0]))
+#         pyscript.write('o2', int(path[0][1]))
+#         pyscript.write('o3', int(path[0][2]))
+#         pyscript.write('o4', int(path[0][3]))
+#         pyscript.write('o5', int(path[1][0]))
+#         pyscript.write('o6', int(path[1][1]))
+#         pyscript.write('o7', int(path[1][2]))
+#         pyscript.write('o8', int(path[1][3]))
+#         pyscript.write('o9', int(path[2][0]))
+#         pyscript.write('o10', int(path[2][1]))
+#         pyscript.write('o11', int(path[2][2]))
+#         pyscript.write('o12', int(path[2][3]))
+#         pyscript.write('o13', int(path[3][0]))
+#         pyscript.write('o14', int(path[3][1]))
+#         pyscript.write('o15', int(path[3][2]))
+#         pyscript.write('o16', int(path[3][3]))
+#     else:
+#         pyscript.write('solveStatus', 'The board could not be solved :(')
